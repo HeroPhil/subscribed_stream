3e29b768f1ccbe6c28a31a8394d17650ecbcf2ae
herophil <philip.herold.01@gmail.com>
2021-11-26 10:25:12 +0100

    added subscribed stream class

--- lib/subscribed_stream.dart
+++ lib/subscribed_stream.dart
@@ -1,7 +1,45 @@
 library subscribed_stream;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+typedef T StreamSubscriptionCallback<T>(
+  T data,
+  T? previous,
+  SubscribedStream<T> self,
+);
+
+/// Bean class holding a stream and corresponding subscriptions
+class SubscribedStream<T> {
+  Stream<T> stream;
+  List<StreamSubscription<T>?> subscriptions = [];
+  T? latestValue;
+  StreamSubscriptionCallback<T> onStreamEvent;
+
+  SubscribedStream({
+    required this.stream,
+    required this.onStreamEvent,
+    this.latestValue,
+    bool initiallySubscribed = true,
+  }) {
+    if (initiallySubscribed) {
+      subscribe();
+    }
+  }
+
+  void subscribe() {
+    subscriptions.addAll([
+      stream.listen((event) {
+        latestValue = onStreamEvent.call(event, latestValue, this);
+      })
+    ]);
+  }
+
+  Future unsubscribe() {
+    return Future.wait(
+      subscriptions.map(
+        (subscription) => subscription?.cancel() ?? Future(() {}),
+      ),
+    );
+  }
 }
+
