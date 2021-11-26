619081682a48f3173fb6a4bae53adf468330cb21
herophil <philip.herold.01@gmail.com>
2021-11-26 10:38:00 +0100

    added code comments

--- lib/subscribed_stream.dart
+++ lib/subscribed_stream.dart
@@ -3,22 +3,37 @@ library subscribed_stream;
 import 'dart:async';
 
 typedef T StreamSubscriptionCallback<T>(
+  /// The latest data emitted by the stream.
   T data,
+  /// The previous data emitted by the stream.
   T? previous,
+  /// This object itself. Use this to i.e. unsubscribe from the stream.
   SubscribedStream<T> self,
 );
 
-/// Bean class holding a stream and corresponding subscriptions
+/// Utility class for subscribing to a stream.
+/// 
+/// The [stream] is a [Stream] that emits [T] values.
+/// The [onStreamEvent] callback is called whenever a new value is emitted.
 class SubscribedStream<T> {
+  /// The stream which is being subscribed to
   Stream<T> stream;
+  /// A list of subscriptions to the stream
   List<StreamSubscription<T>?> subscriptions = [];
+  /// The last emitted data of type [T]
   T? latestValue;
+  /// Callback to be called when a new value is emitted by the stream
   StreamSubscriptionCallback<T> onStreamEvent;
 
+  /// Constructor
   SubscribedStream({
+    /// The stream to subscribe to
     required this.stream,
+    /// The callback to be called when a new value is emitted by the stream
     required this.onStreamEvent,
+    /// The initial value to be used as latestValue. Default to null.
     this.latestValue,
+    /// Whether to automatically subscribe to the stream. Default to true.
     bool initiallySubscribed = true,
   }) {
     if (initiallySubscribed) {
@@ -26,6 +41,11 @@ class SubscribedStream<T> {
     }
   }
 
+  /// Subscribe to the stream with the [onStreamEvent] callback.
+  /// 
+  /// Is being called on construction if [initiallySubscribed] is true.
+  /// If this is called multiple times and [unsubscribe] is not called,
+  /// the stream will be subscribed multiple times.
   void subscribe() {
     subscriptions.addAll([
       stream.listen((event) {
@@ -34,6 +54,9 @@ class SubscribedStream<T> {
     ]);
   }
 
+  /// Cancels all subscriptions to the stream.
+  /// 
+  /// Returns a [Future] which completes when all subscriptions are cancelled.
   Future unsubscribe() {
     return Future.wait(
       subscriptions.map(
