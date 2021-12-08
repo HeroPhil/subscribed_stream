c3b6b8bd8caeb86f6fff324677b545059e930202
herophil <philip.herold.01@gmail.com>
2021-12-08 09:23:07 +0100

    update formating

--- lib/subscribed_stream.dart
+++ lib/subscribed_stream.dart
@@ -5,23 +5,28 @@ import 'dart:async';
 typedef T StreamSubscriptionCallback<T>(
   /// The latest data emitted by the stream.
   T data,
+
   /// The previous data emitted by the stream.
   T? previous,
+
   /// This object itself. Use this to i.e. unsubscribe from the stream.
   SubscribedStream<T> self,
 );
 
 /// Utility class for subscribing to a stream.
-/// 
+///
 /// The [stream] is a [Stream] that emits [T] values.
 /// The [onStreamEvent] callback is called whenever a new value is emitted.
 class SubscribedStream<T> {
   /// The stream which is being subscribed to
   Stream<T> stream;
+
   /// A list of subscriptions to the stream
   List<StreamSubscription<T>?> subscriptions = [];
+
   /// The last emitted data of type [T]
   T? latestValue;
+
   /// Callback to be called when a new value is emitted by the stream
   StreamSubscriptionCallback<T> onStreamEvent;
 
@@ -29,10 +34,13 @@ class SubscribedStream<T> {
   SubscribedStream({
     /// The stream to subscribe to
     required this.stream,
+
     /// The callback to be called when a new value is emitted by the stream
     required this.onStreamEvent,
+
     /// The initial value to be used as latestValue. Default to null.
     this.latestValue,
+
     /// Whether to automatically subscribe to the stream. Default to true.
     bool initiallySubscribed = true,
   }) {
@@ -42,7 +50,7 @@ class SubscribedStream<T> {
   }
 
   /// Subscribe to the stream with the [onStreamEvent] callback.
-  /// 
+  ///
   /// Is being called on construction if [initiallySubscribed] is true.
   /// If this is called multiple times and [unsubscribe] is not called,
   /// the stream will be subscribed multiple times.
@@ -55,7 +63,7 @@ class SubscribedStream<T> {
   }
 
   /// Cancels all subscriptions to the stream.
-  /// 
+  ///
   /// Returns a [Future] which completes when all subscriptions are cancelled.
   Future unsubscribe() {
     return Future.wait(
@@ -65,4 +73,3 @@ class SubscribedStream<T> {
     );
   }
 }
-
